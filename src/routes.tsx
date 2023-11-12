34bf8f16574e81f18959da9e5d7d0b24ffa7c03d
joaorodrigues2012 <joaorodrigues2012.jr@gmail.com>
2023-11-12 20:50:31 -0300

    teste pwa em expo

---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Main from "./pages/Main";
+import Camera from "./pages/Camera";
+import { Feather } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+        options={{ tabBarIcon: ({color}) => <Feather name="home" size={20} color={color} /> }}
+         name="Main"
+        component={Main} />
+        <Tab.Screen 
+        options={{ tabBarIcon: ({color}) => <Feather name="camera" size={20} color={color} /> }}
+        name="Camera"
+         component={Camera} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
